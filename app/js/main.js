795acce733a557c00133921da68cbeef987ad00d
Butterbroad <pyatochevskiy@gmail.com>
2019-12-15 18:21:06 +0300

    added section surf

--- app/js/main.js
+++ app/js/main.js
@@ -3,17 +3,32 @@ $(function(){
 $('.header__slider').slick({
     infinite: true,
     fade: true,
-    asNavFor: '.slider-dots',
+    asNavFor: '.slider-dotshead',
     prevArrow: '<img class="slider-arrows slider-arrows-left" src="img/arrow-left.svg" alt=""></img>',
     nextArrow: '<img class="slider-arrows slider-arrows-right" src="img/arrow-right.svg" alt=""></img>',
 });
 
-    $('.slider-dots').slick({
+    $('.slider-dotshead').slick({
         slidesToShow: 4,
         slidesToScroll: 4,
         asNavFor: '.header__slider',
         dots: true,
     });
 
+$('.surf-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<img class="slider-arrows slider-arrows-left" src="img/arrow-left.svg" alt=""></img>',
+    nextArrow: '<img class="slider-arrows slider-arrows-right" src="img/arrow-right.svg" alt=""></img>',
+    asNavFor: '.slider-map',
+});   
+
+$('.slider-map').slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.surf-slider',
+    focusOnSelect: true,
+});
   
 });
